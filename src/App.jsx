fae6405085357e32d47c7e97a0454f9499df0ec1
PoPodada <goito.722@icloud.com>
2024-03-08 16:08:04 +0900

    material-uiライブラリのtestを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-
+import TextField from '@mui/material/TextField';
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
         ビギナーズハッカソン
       </h1>
+      <TextField id="outlined-search" label="メールアドレスを入力する" type="search" />
     </div>
   );
 }
