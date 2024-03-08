da720c2234d3584c173fe20ec142b9f6d1136dac
SEKI-YUTA <zard.since1991@gmail.com>
2024-03-08 16:36:30 +0900

    デモのトラックデータを追加

---
+++ src/DemoData.js
@@ -0,0 +1,8 @@
+const DEMO_TRACK_DATA = {
+    trackName: "Demo Track",
+    trackId: "123456",
+    trackLength: 180,
+    description: "This is a demo track. It is not real. It is only a test.",
+    jacket: "https://dummyimage.com/400x400/000/fff",
+    links: ["https://www.example.com", "https://www.example2.com"],
+};
