4643eb248ab0ec7521f1e77146e5afe2b772789a
PoPodada <goito.722@icloud.com>
2024-03-08 19:17:48 +0900

    App.jsxのいらないタグを削除

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import { LoginScreen } from './pages/LoginScreen';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        ビギナーズハッカソン
-      </h1>
       <LoginScreen/>
     </div>
   );
