0e68348c96f608eb264a07bc6b37bda217341d29
PoPodada <goito.722@icloud.com>
2024-03-09 11:08:20 +0900

    eslintのエラーを修正

--- src/pages/MainScreen.jsx
+++ src/pages/MainScreen.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { firebaseApp,auth,provider } from '../firebase';
-import { signInWithEmailAndPassword,createUserWithEmailAndPassword,getAuth } from "firebase/auth";
-
 const MainScreen = () => {
     return (
         <div>
