2b86c11a9e3853f75e78e33264737cc555d96f2d
SEKI-YUTA <zard.since1991@gmail.com>
2024-03-09 21:34:32 +0900

    メイン画面のUIを修正

--- src/pages/mainScreen/mainScreen.jsx
+++ src/pages/mainScreen/mainScreen.jsx
@@ -1,62 +1,49 @@
 import DEMO_TRACK_DATA from "../../DemoData";
 import { FaGear } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import SmallTrackItem from "../../components/SmallTrackItem";
+import AppHeader from "../../components/AppHeader";
 
 function Mainscreen() {
     return (
-        <div className="w-full space-between ">
-            <div className="flex w-full">
-                <div className="flex-none grow">
-                    <div className="m-11 ">
-                        <img
-                            className="size-max"
-                            src={DEMO_TRACK_DATA.jacket}
-                        />
-                    </div>
-                    <div className="ml-11 rounded-md">
-                        <h1 className="font-sans text-4xl mt-4 mb-4 ml-2">
-                            {DEMO_TRACK_DATA.trackName}
-                        </h1>
-                    </div>
-                </div>
-
-                <div className="flex-initial grow">
-                    <div className="flex mt-8 ml-11 w-[500px] h-[20px] border-b">
-                        <p className="ml-7 text-left">
-                            {DEMO_TRACK_DATA.artistName}
-                        </p>
-                    </div>
-
-                    <div className="m-11 border-2 rounded-md">
-                        <p className="h-40 overflow-auto">
-                            {DEMO_TRACK_DATA.description}
-                        </p>
-                    </div>
-                    <div className="ml-11 mr-11">
-                        <div className="md-4">
-                            {(() => {
-                                return DEMO_TRACK_DATA.links.map((link) => {
-                                    return (
-                                        <div className="h-20 mt-7 md-7 border-2 rounded-md">
-                                            <p className=" mr-7">DemoSite</p>
-                                            <a
-                                                className="underline text-cyan-400"
-                                                href={link}
-                                            >
-                                                {link}
-                                            </a>
-                                        </div>
-                                    );
-                                });
-                            })()}
+        <div>
+            <AppHeader tailIcon={<FaGear size={26} />} />
+            <div className="w-full space-between ">
+                <div className="flex size-full">
+                    <div className="flex-1 grow h-max ml-10 mr-10">
+                        <div>
+                            <img
+                                className="size-full aspect-square rounded-lg"
+                                src={DEMO_TRACK_DATA.jacket}
+                            />
+                        </div>
+                        <div className="rounded-md text-start">
+                            <h1 className="font-sans text-4xl mt-4 ml-2">
+                                {DEMO_TRACK_DATA.trackName}
+                            </h1>
                         </div>
                     </div>
-                </div>
 
-                <div className="flex-initial grow mt-11">
-                    <a href="">
-                        <FaGear />
-                    </a>
+                    <div className="flex-1 grow flex flex-col mr-10">
+                        <div className="flex w-[500px] border-b">
+                            <p className="ml-3 text-left text-2xl">
+                                {DEMO_TRACK_DATA.artistName}
+                            </p>
+                            <div className="w-10 h-10 rounded-full"></div>
+                        </div>
+
+                        <div className="mt-4 border-2 rounded-md text-start p-2 flex-1">
+                            <p className="h-40 overflow-auto">
+                                {DEMO_TRACK_DATA.description}
+                            </p>
+                        </div>
+                        {/* おすすめの曲リスト */}
+                        <div className="flex">
+                            <SmallTrackItem />
+                            <SmallTrackItem />
+                            <SmallTrackItem />
+                            <SmallTrackItem />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
