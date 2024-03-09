1ba038a2478837e393de639810446f4ac4345d84
Mirai0930 <s2370012@s.do-johodai.ac.jp>
2024-03-09 12:52:51 +0900

    [add]機能追加

--- src/pages/mainScreen/mainScreen.jsx
+++ src/pages/mainScreen/mainScreen.jsx
@@ -1,24 +1,51 @@
 import DEMO_TRACK_DATA from "../../DemoData";
+import { FaGear } from "react-icons/fa6";
 
 function Mainscreen() {
   return (
     
+    
     <div className="w-full space-between">
         <div className="flex w-full">
             <div className="flex-none grow">
-                <div className="container m-12">
-                    <img className="size-96" src={DEMO_TRACK_DATA.jacket}/>
-                    <h1 className="mt-10 text-7xl">{DEMO_TRACK_DATA.trackName}</h1>
-                </div>
-            </div>
-            <div className="flex-initial grow">
-                <div className="container m-11">
-                    <div className="w-96 h-24">
-                        <p>{DEMO_TRACK_DATA.description}</p>
+                <div className="m-11 ">
+                    <img className="size-max" src={DEMO_TRACK_DATA.jacket}/>
                     </div>
-                    
-                </div>
+                    <div className="ml-11 border-2 rounded-md">
+                        <h1 className="font-sans text-4xl mt-4 mb-4 ml-2">{DEMO_TRACK_DATA.trackName}</h1>
+                    </div>
             </div>
+
+            <div className="flex-initial grow">
+                    <div className="m-11 border-2 rounded-md">
+                        <p className="h-40">{DEMO_TRACK_DATA.description}</p>
+                    </div>
+                    <div className="ml-11 mr-11">
+                         <div className="md-4">
+                            {
+                                (() => {
+                                    return (
+                                        DEMO_TRACK_DATA.links.map((link) => {
+                                            return (
+                                            <div className ="h-20 mt-7 md-7 border-2 rounded-md">
+                                            <p className="ml-7 mr-7">DemoSite</p>
+                                            <a className="underline text-cyan-400"href = {link}>{link}</a>
+                                            </div>
+                                            )
+                                        })
+                                    );
+                                })()
+
+                            }
+                         </div>
+                    </div>
+            </div>
+
+            
+            <div className="flex-initial grow mt-11" >
+                <a href=""><FaGear /></a>
+            </div>
+
 
         </div>
         
@@ -27,4 +54,5 @@ function Mainscreen() {
 }
 
 export default Mainscreen;
+
 
