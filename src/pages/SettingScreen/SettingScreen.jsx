9301e20b4c82c97c86d1d65a4f9dc333ee868f59
SEKI-YUTA <zard.since1991@gmail.com>
2024-03-11 09:27:32 +0900

    おすすめ設定のデフォルトを好きなジャンル以外に変更

--- src/pages/SettingScreen/SettingScreen.jsx
+++ src/pages/SettingScreen/SettingScreen.jsx
@@ -34,8 +34,8 @@ const SettingScreen = () => {
             </div>
             <div className="inline-block relative">
                 <select className="appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                    <option>好きなジャンルの曲を表示</option>
                     <option>好きなジャンル以外の曲を表示</option>
+                    <option>好きなジャンルの曲を表示</option>
                 </select>
                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg
